845584b8867175eb7c37ba6948d36d6b8a22f022
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-31 00:43:52 +0300

    Update main.h

--- main.h
+++ main.h
@@ -23,6 +23,7 @@ int _print_invalid_spec(char prev_format, char format, int count);
 int _print_a_integer(va_list args);
 void _recursion_integer(int a);
 int _print_int_binary(va_list args);
+int _print_bin(va_list args);
 void _recursion_int_binary(int a);
 int _validate_char(char _type);
 
