d28e5cfa93e0540073348fd8a904440453593dc6
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-31 01:07:27 +0300

    Update main.h

--- main.h
+++ main.h
@@ -9,12 +9,12 @@
   */
 typedef struct specifiers
 {
-    char *specifier;
-    int (*f)(va_list args);
+	char *specifier;
+	int (*f)(va_list args);
 } spc_dt;
 
-int _printf(const char *format, ...);
 int _write(char c);
+int _printf(const char *format, ...);
 int _print_a_char(va_list args);
 int _print_a_string(va_list args);
 int _print_format(const char *format, va_list args);
@@ -23,8 +23,7 @@ int _print_invalid_spec(char prev_format, char format, int count);
 int _print_a_integer(va_list args);
 void _recursion_integer(int a);
 int _print_int_binary(va_list args);
-int _print_bin(va_list args);
 void _recursion_int_binary(int a);
 int _validate_char(char _type);
 
-#endif /* MAIN_H */
+#endif /* MAIN_H*/
